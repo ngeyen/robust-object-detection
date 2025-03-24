c0f2eefa83e52a236c1afcd2b3b4b6c59eaf7972
Ngeyen Victory <ngeyen@cmu.edu>
2025-03-24 12:52:06 +0200

    Run inference skeleton

--- src/inference/run_inference.py
+++ src/inference/run_inference.py
@@ -1,1 +1,11 @@
-# Placeholder for run_inference.py
+from models.yolov5_nano import run as run_yolo
+
+# Read an image
+
+# perform the necessary transformation on the image except for yolo
+
+# Run inference
+
+# display the image with bounding box within experiments directory
+
+
